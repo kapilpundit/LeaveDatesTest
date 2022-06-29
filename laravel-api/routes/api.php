6bd9a4917f8359d2f067536069ce4dc72f876339
Kapil Sharma <kapil.sharma@fulcrumdigital.com>
2022-06-29 19:43:21 +0530

    Added '/' route

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get(
+//     '/user', function (Request $request) {
+//         return $request->user();
+//     }
+// );
+
+Route::get('/', function (Request $request) {
+    return response()->json(
+        [
+            'message' => 'Welcome to Leave Dates.'
+        ],
+        200
+    );
 });
